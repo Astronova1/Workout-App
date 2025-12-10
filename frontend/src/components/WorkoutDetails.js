e7b0456cbadf205c4a47d4d78b7a763409ed3ff5
Awaiz <awaiz123nasir123@gmail.com>
2025-12-10 20:54:20 +0500

    fixed nested frontend repo

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,29 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutContext"
+
+const WorkoutDetails = ({ workout }) => {
+
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('https://exercise-app-neon.vercel.app' + workout._id, {      ///api/workouts/
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok){
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+
+    return (
+        <div className="workout-details">
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg):</strong>{workout.load}</p>
+            <p><strong>Reps: </strong>{workout.reps}</p>
+            <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
+        </div>
+    )
+}
+
+export default WorkoutDetails
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
+
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from "../components/workoutForm";
+
+const Home = () => {
+    const { workouts, dispatch} = useWorkoutsContext(); 
+
+    useEffect(() => {
+        const fetchWorkouts = async() =>{
+            const response = await fetch('https://exercise-app-neon.vercel.app')
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+
+        fetchWorkouts()
+
+    },[dispatch])
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) =>(
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
+            <WorkoutForm/>
+        </div>
+    )
+}
+
+export default Home;
